08d59c3391ec46b22bf0e6718e24d7730d31cbf7
Dylan William Hardison <dylan@hardison.net>
2019-07-05 19:37:40 -0400

    add support for gobootstrap on arm
    On arm platform, we need to gobootstrap from 1.4.

--- Formula/go.rb
+++ Formula/go.rb
@@ -37,11 +37,18 @@ class Go < Formula
     if OS.mac?
       url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
       sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
+      version "1.7"
     elsif OS.linux?
-      url "https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz"
-      sha256 "702ad90f705365227e902b42d91dd1a40e48ca7f67a2f4b2fd052aaa4295cd95"
+      if Hardware::CPU.intel?
+        url "https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz"
+        sha256 "702ad90f705365227e902b42d91dd1a40e48ca7f67a2f4b2fd052aaa4295cd95"
+        version "1.7"
+      elsif Hardware::CPU.arm?
+        url "https://raw.githubusercontent.com/tgogos/rpi_golang/master/go1.4.3.linux-armv7.tar.gz"
+        sha256 "dc1f98ca1b5476f4cf4d5e41df265a69b6fc8ede5664fb5c227d9ca5ed1720ce"
+        version "1.4"
+      end
     end
-    version "1.7"
   end
 
   # Prevents Go from building malformed binaries. Fixed upstream, should
@@ -83,7 +90,12 @@ class Go < Formula
     libexec.install Dir["*"]
     bin.install_symlink Dir[libexec/"bin/go*"]
 
-    system bin/"go", "install", "-race", "std"
+    # technically -race is ok on arm64
+    if Hardware::CPU.arm?
+      system bin/"go", "install", "std"
+    else
+      system bin/"go", "install", "-race", "std"
+    end
 
     # Build and install godoc
     ENV.prepend_path "PATH", bin
